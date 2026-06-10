cc5b30afa9e8d2f3021462c5abda4cdddfce74b8
LUMI <lumi@sias-tagebuch>
2026-06-10 08:31:16 +0200

    🧹 Netlify entfernt, HTML überarbeitet – bereit für GitHub Pages

--- _deploy_now.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd /d "D:\Sias Tagebuch"
-set NETLIFY_AUTH_TOKEN=***
-netlify deploy --prod --skip-functions-cache
-echo.
-echo Fertig!
--- deploy.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set NETLIFY_AUTH_TOKEN=nfp_RDQAKsExYXexENXFc3N5xMeeT6fno4d64507
-cd /d "D:\Sias Tagebuch"
-netlify deploy --prod --skip-functions-cache
-echo.
-echo Fertig! Fehler-Code: %ERRORLEVEL%
-pause
